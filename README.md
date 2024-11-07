483accd3ca3fa2c59edece4d4235ec4353c0863b
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2024-11-07 23:47:59 +0000

    Add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 repository for the curso.dev project
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Wedja, I love you 😘</h1>
+function Home() {
+  return <h1>Wedja, I love you 😘</h1>;
 }
 
 export default Home;
