c4509a275eb3c245226f3f2362ffa767ec9126c0
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2024-11-29 21:46:23 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  var result = await database.query("SELECT 1 + 1 AS sum");
+  console.log(result.rows);
   response.status(200).json({ status: "This course is amazing" });
 }
 
