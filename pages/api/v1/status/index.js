6f47c6f4e52de2f92537bee425ea639ca36dd407
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2024-12-01 18:23:45 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  var result = await database.query("SELECT 1 + 1 AS sum");
-  console.log(result.rows);
-  response.status(200).json({ status: "This course is amazing" });
+  var updatedAt = new Date().toISOString();
+
+  var databaseVersionResult = await database.query("SHOW server_version;");
+  var databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  var databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  var databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  var databaseName = process.env.POSTGRES_DB;
+  var databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  var databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
