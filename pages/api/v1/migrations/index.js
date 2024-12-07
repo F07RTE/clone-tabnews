c5d349ac9e5d4d5031a09f46b15bf40626975997
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2024-12-07 13:49:38 +0000

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,37 +3,71 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  var dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
 
-  var defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
+    });
+  }
 
   if (request.method === "GET") {
-    var pendingMigrations = await migrationsRunner(defaultMigrationOptions);
-    dbClient.end();
-
-    return response.status(200).json(pendingMigrations);
+    return await get(response);
   }
 
   if (request.method === "POST") {
+    return await post(response);
+  }
+}
+
+async function get(response) {
+  try {
+    var dbClient = await database.getNewClient();
+    var defaultMigrationOptions = createDefaultMigrationOptions(dbClient);
+
+    var pendingMigrations = await migrationsRunner({
+      ...defaultMigrationOptions,
+      dryRun: true,
+    });
+
+    return response.status(200).json(pendingMigrations);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
+}
+
+async function post(response) {
+  try {
+    var dbClient = await database.getNewClient();
+    var defaultMigrationOptions = createDefaultMigrationOptions(dbClient);
+
     var migratedMigrations = await migrationsRunner({
       ...defaultMigrationOptions,
       dryRun: false,
     });
-    dbClient.end();
 
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
 
     return response.status(200).json(migratedMigrations);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
   }
+}
 
-  return response.status(405).end();
+function createDefaultMigrationOptions(dbClient) {
+  return {
+    dbClient: dbClient,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 }
