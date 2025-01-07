4f58c23c8f8aa3eb095223e6ffa9e75323f0964c
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2025-01-07 12:42:15 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -10,12 +10,16 @@ async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
-test("GET to /api/v1/migration should return 200", async () => {
-  var response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migration", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      var response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  var responseBody = await response.json();
+      var responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
