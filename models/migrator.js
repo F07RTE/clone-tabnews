95dcfb3642d79dba4dd0a3de2d2933477103a9d9
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2025-02-16 21:40:36 +0000

    feat: add `models/migrator.js`

---
+++ models/migrator.js
@@ -0,0 +1,65 @@
+import migrationsRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database.js";
+import { ServiceError } from "infra/errors";
+
+function createDefaultMigrationOptions(dbClient) {
+  return {
+    dbClient: dbClient,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+}
+
+async function listPendingMigrations() {
+  try {
+    var dbClient = await database.getNewClient();
+    var defaultMigrationOptions = createDefaultMigrationOptions(dbClient);
+
+    var pendingMigrations = await migrationsRunner({
+      ...defaultMigrationOptions,
+      dryRun: true,
+    });
+
+    return pendingMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Error when listing pending migrations.",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  } finally {
+    dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  try {
+    var dbClient = await database.getNewClient();
+    var defaultMigrationOptions = createDefaultMigrationOptions(dbClient);
+
+    var migratedMigrations = await migrationsRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Error when running pending migrations.",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  } finally {
+    dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
