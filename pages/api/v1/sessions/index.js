350a712595ad3c969fa815068cb44b35596a8abe
Guilherme Forte <guilherme.forte17@gmail.com>
2026-06-20 16:33:37 -0300

    feat: implement session deletion

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -7,6 +7,7 @@ import session from "models/session.js";
 const router = createRouter();
 
 router.post(postHandler);
+router.delete(deleteHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -23,4 +24,15 @@ async function postHandler(request, response) {
   controller.setSessionCookie(createdSession.token, response);
 
   return response.status(201).json(createdSession);
+}
+
+async function deleteHandler(request, response) {
+  const sessionToken = request.cookies.session_id;
+
+  const sessionFromDb = await session.findOneValidByToken(sessionToken);
+  const expiredSession = await session.expireById(sessionFromDb.id);
+
+  controller.clearSessionCookie(response);
+
+  return response.status(200).json(expiredSession);
 }
